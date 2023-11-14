fa1adcd1bf7c8af8dc355edba8d2691f088add46
LeonardoBoni <123012608+LeonardoBoni@users.noreply.github.com>
2023-11-14 14:00:41 -0300

    Update server.ino

--- server.ino
+++ server.ino
@@ -1,72 +1,73 @@
-//==================================================================================//
-// SERVER                                                                           //
-//----------------------------------------------------------------------------------//
-
-//------------------------------------------------------------------------------------
-// Bibliotecas necessárias
-//------------------------------------------------------------------------------------
 #include <SPI.h>
 #include <ESP8266WiFi.h>
-#include <DHT.h> // Biblioteca do sensor DHT
-
-//------------------------------------------------------------------------------------
-// WIFI
-//------------------------------------------------------------------------------------
-char ssid2[]= "Automacao";
-char pass2[]= "Senaisp901";
-
+#include <DHT.h>
 
 char ssid[] = "FVML";
 char pass[] = "fvml1234";
+
 WiFiServer server(80);
 
-  IPAddress ip(192, 168, 10, 40);            // IP fixo do server
-  IPAddress gateway(192, 168, 10, 1);        // gateway do server
-  IPAddress subnet(255, 255, 255, 0);        // subrede do server
+IPAddress ip(192, 168, 10, 40);
+IPAddress gateway(192, 168, 10, 1);
+IPAddress subnet(255, 255, 255, 0);
 
-//====================================================================================
+DHT dht(2, DHT22); // Use o mesmo pino que você usou no cliente
+
 void setup() {
-  //Criação de comunicação serial para debug
   Serial.begin(115200);
-  
-  WiFi.mode(WIFI_AP_STA);                    // envio de comandos por TCP/IP
-
-  //Configurando a rede
+  WiFi.mode(WIFI_AP_STA);
   WiFi.softAPConfig(ip, gateway, subnet);
   WiFi.softAP(ssid, pass);
   delay(500);
-  //Iniciando o Server
   server.begin();
 
-//------------------------------------------------------------------------------------
-// Parametros da rede - Somente para debug - Pode apagar
-//------------------------------------------------------------------------------------  
   Serial.println("ESP Server Initialized");
-  Serial.print("IP: ");       Serial.println(WiFi.softAPIP());
-  Serial.print("SSID: ");     Serial.println(WiFi.SSID());
-  Serial.print("Signal: ");   Serial.println(WiFi.RSSI());
+  Serial.print("IP: "); Serial.println(WiFi.softAPIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
 }
 
 void loop() {
-  //Verificando se possui um cliente conectado
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
 
-  //recebe os dados vindo do cliente
   String request = client.readStringUntil('\r');
   client.flush();
 
-  //Mostra os dados vindo do cliente
-  Serial.print("Data Received: "); Serial.println(request);
-  delay(200);
-   String request2 = client.readStringUntil('hum');
-   Serial.println(request2);
+  Serial.print("Data Received: "); 
+  Serial.println(request);
 
- 
+  // Gere uma página HTML com os dados do sensor
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-  //Envia os dados para o cliente
-  client.print("I am server");
+   String html = "<html><head>";
+    html += "<style>";
+    html += "body { background-color: read; text-align: center; }";
+    html += "h1 { color: white; }";
+    html += "p { font-size: 30px; }";
+    html += "</style>";
+    html += "</head><body>";
+    html += 
+   
+"<h1>Dados do Sensor</h1>";
+    html += "<p>Umidade: " + String(h) + "%</p>";
+    html += 
+   
+"<p>Temperatura: " + String(t) + "*C</p>";
+    html += 
+    html +=
+
+    html;
+"</body></html>";
+
+  // Envie a página HTML como resposta
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");
+  client.println();
+  client.println(html);
   delay(200);
 }
